7936dc2a3b64dd26e5fe7ec98cc8ca66d1bee241
Indibar <indibar@algo2trade.com>
2020-08-29 22:31:39 +0530

    After 4.0.0.0

--- ScoreCalculator/frmImpactChange.Designer.vb
+++ ScoreCalculator/frmImpactChange.Designer.vb
@@ -23,20 +23,20 @@ Partial Class frmImpactChange
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.grpDAAI = New System.Windows.Forms.GroupBox()
-        Me.grpQET = New System.Windows.Forms.GroupBox()
-        Me.grpMS = New System.Windows.Forms.GroupBox()
-        Me.grpDX = New System.Windows.Forms.GroupBox()
-        Me.grpUNIX = New System.Windows.Forms.GroupBox()
-        Me.grpMF = New System.Windows.Forms.GroupBox()
-        Me.grpDBI = New System.Windows.Forms.GroupBox()
-        Me.btnSubmit = New System.Windows.Forms.Button()
-        Me.ucImpactUNIX = New ScoreCalculator.ucImpact()
-        Me.ucImpactMF = New ScoreCalculator.ucImpact()
-        Me.ucImpactDBI = New ScoreCalculator.ucImpact()
-        Me.ucImpactDX = New ScoreCalculator.ucImpact()
-        Me.ucImpactMS = New ScoreCalculator.ucImpact()
-        Me.ucImpactQET = New ScoreCalculator.ucImpact()
         Me.ucImpactDAAI = New ScoreCalculator.ucImpact()
+        Me.grpQET = New System.Windows.Forms.GroupBox()
+        Me.ucImpactQET = New ScoreCalculator.ucImpact()
+        Me.grpMS = New System.Windows.Forms.GroupBox()
+        Me.ucImpactMS = New ScoreCalculator.ucImpact()
+        Me.grpDX = New System.Windows.Forms.GroupBox()
+        Me.ucImpactDX = New ScoreCalculator.ucImpact()
+        Me.grpUNIX = New System.Windows.Forms.GroupBox()
+        Me.ucImpactUNIX = New ScoreCalculator.ucImpact()
+        Me.grpMF = New System.Windows.Forms.GroupBox()
+        Me.ucImpactMF = New ScoreCalculator.ucImpact()
+        Me.grpDBI = New System.Windows.Forms.GroupBox()
+        Me.ucImpactDBI = New ScoreCalculator.ucImpact()
+        Me.btnSubmit = New System.Windows.Forms.Button()
         Me.grpDAAI.SuspendLayout()
         Me.grpQET.SuspendLayout()
         Me.grpMS.SuspendLayout()
@@ -56,6 +56,13 @@ Partial Class frmImpactChange
         Me.grpDAAI.TabStop = False
         Me.grpDAAI.Text = "DAAI"
         '
+        'ucImpactDAAI
+        '
+        Me.ucImpactDAAI.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactDAAI.Name = "ucImpactDAAI"
+        Me.ucImpactDAAI.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactDAAI.TabIndex = 0
+        '
         'grpQET
         '
         Me.grpQET.Controls.Add(Me.ucImpactQET)
@@ -65,6 +72,13 @@ Partial Class frmImpactChange
         Me.grpQET.TabIndex = 1
         Me.grpQET.TabStop = False
         Me.grpQET.Text = "QET"
+        '
+        'ucImpactQET
+        '
+        Me.ucImpactQET.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactQET.Name = "ucImpactQET"
+        Me.ucImpactQET.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactQET.TabIndex = 0
         '
         'grpMS
         '
@@ -76,6 +90,13 @@ Partial Class frmImpactChange
         Me.grpMS.TabStop = False
         Me.grpMS.Text = "MS"
         '
+        'ucImpactMS
+        '
+        Me.ucImpactMS.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactMS.Name = "ucImpactMS"
+        Me.ucImpactMS.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactMS.TabIndex = 0
+        '
         'grpDX
         '
         Me.grpDX.Controls.Add(Me.ucImpactDX)
@@ -85,6 +106,13 @@ Partial Class frmImpactChange
         Me.grpDX.TabIndex = 3
         Me.grpDX.TabStop = False
         Me.grpDX.Text = "DX"
+        '
+        'ucImpactDX
+        '
+        Me.ucImpactDX.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactDX.Name = "ucImpactDX"
+        Me.ucImpactDX.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactDX.TabIndex = 0
         '
         'grpUNIX
         '
@@ -96,6 +124,13 @@ Partial Class frmImpactChange
         Me.grpUNIX.TabStop = False
         Me.grpUNIX.Text = "UNIX"
         '
+        'ucImpactUNIX
+        '
+        Me.ucImpactUNIX.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactUNIX.Name = "ucImpactUNIX"
+        Me.ucImpactUNIX.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactUNIX.TabIndex = 0
+        '
         'grpMF
         '
         Me.grpMF.Controls.Add(Me.ucImpactMF)
@@ -105,6 +140,13 @@ Partial Class frmImpactChange
         Me.grpMF.TabIndex = 5
         Me.grpMF.TabStop = False
         Me.grpMF.Text = "MF"
+        '
+        'ucImpactMF
+        '
+        Me.ucImpactMF.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactMF.Name = "ucImpactMF"
+        Me.ucImpactMF.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactMF.TabIndex = 0
         '
         'grpDBI
         '
@@ -116,6 +158,13 @@ Partial Class frmImpactChange
         Me.grpDBI.TabStop = False
         Me.grpDBI.Text = "DBI"
         '
+        'ucImpactDBI
+        '
+        Me.ucImpactDBI.Location = New System.Drawing.Point(6, 22)
+        Me.ucImpactDBI.Name = "ucImpactDBI"
+        Me.ucImpactDBI.Size = New System.Drawing.Size(494, 62)
+        Me.ucImpactDBI.TabIndex = 0
+        '
         'btnSubmit
         '
         Me.btnSubmit.Location = New System.Drawing.Point(923, 374)
@@ -124,55 +173,6 @@ Partial Class frmImpactChange
         Me.btnSubmit.TabIndex = 7
         Me.btnSubmit.Text = "Submit"
         Me.btnSubmit.UseVisualStyleBackColor = True
-        '
-        'ucImpactUNIX
-        '
-        Me.ucImpactUNIX.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactUNIX.Name = "ucImpactUNIX"
-        Me.ucImpactUNIX.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactUNIX.TabIndex = 0
-        '
-        'ucImpactMF
-        '
-        Me.ucImpactMF.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactMF.Name = "ucImpactMF"
-        Me.ucImpactMF.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactMF.TabIndex = 0
-        '
-        'ucImpactDBI
-        '
-        Me.ucImpactDBI.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactDBI.Name = "ucImpactDBI"
-        Me.ucImpactDBI.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactDBI.TabIndex = 0
-        '
-        'ucImpactDX
-        '
-        Me.ucImpactDX.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactDX.Name = "ucImpactDX"
-        Me.ucImpactDX.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactDX.TabIndex = 0
-        '
-        'ucImpactMS
-        '
-        Me.ucImpactMS.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactMS.Name = "ucImpactMS"
-        Me.ucImpactMS.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactMS.TabIndex = 0
-        '
-        'ucImpactQET
-        '
-        Me.ucImpactQET.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactQET.Name = "ucImpactQET"
-        Me.ucImpactQET.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactQET.TabIndex = 0
-        '
-        'ucImpactDAAI
-        '
-        Me.ucImpactDAAI.Location = New System.Drawing.Point(6, 22)
-        Me.ucImpactDAAI.Name = "ucImpactDAAI"
-        Me.ucImpactDAAI.Size = New System.Drawing.Size(494, 62)
-        Me.ucImpactDAAI.TabIndex = 0
         '
         'frmImpactChange
         '
@@ -190,6 +190,7 @@ Partial Class frmImpactChange
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmImpactChange"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Impact Change"
         Me.grpDAAI.ResumeLayout(False)
         Me.grpQET.ResumeLayout(False)
